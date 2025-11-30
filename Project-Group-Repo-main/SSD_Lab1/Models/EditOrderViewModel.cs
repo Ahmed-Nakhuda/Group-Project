59b68adcc2e7b7e429cefdeeab7b1bfef2fb5ab6
Ahmed-Nakhuda <144385153+Ahmed-Nakhuda@users.noreply.github.com>
2025-11-30 17:12:19 -0500

    Add EditOrderViewModel class
    Ensure that the application only accepts the data fields intended to be updated by a user to prevent overposting. Not able to edit OrderDate or TotalPrice.

---
+++ Project-Group-Repo-main/SSD_Lab1/Models/EditOrderViewModel.cs
@@ -0,0 +1,14 @@
+namespace SSD_Lab1.Models
+{
+    public class EditOrderViewModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
